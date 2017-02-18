0c7b2257ab1d8aedafcf89a43dd2112871df23b4
Maxim Kim <habamax@gmail.com>
2017-02-18 22:21:10 +0300

    Make strings a bit less green.

--- kosmos-theme.el
+++ kosmos-theme.el
@@ -6,11 +6,10 @@
 
 ;;; Commentary:
 
-;; Sometimes I feel there are too many colors for the code I stare at.
-;; And it looks like I care of only 3 colored things:
-;; 1. Comments -- clearly visible and distinct but not too aggressive (I am looking at you, firebrick!)
-;; 2. Strings  -- did I miss a quote?
-;; 3. Keywords -- a bit of standout
+;; Black and LightGray theme with limited syntax highlighting focusing on:
+;; 1. keywords -- a bit of standout (just white)
+;; 2. strings -- clearly visible, to be sure quotes are closed (greenish)
+;; 3. comments -- should standout of other source code (kind of cyanish).
 
 ;;; Code:
 
@@ -24,7 +23,7 @@
      (kosmos-box-modeline-active "#3f5f5f")
      (kosmos-box-modeline-inactive "#303030")
      (kosmos-keyword "#ffffff")
-     (kosmos-str "#3cb371")
+     (kosmos-str "#73bd7a")
      (kosmos-comment "#5797a0")
      (kosmos-gray "#777777")
      (kosmos-fg-dim "#777777")
